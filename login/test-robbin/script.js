63f66c3dd195fee3b1879804b2963e5ade979a66
robba86 <robbineriksson@gmail.com>
2017-09-29 15:44:47 +0200

    Changed mocup file

--- login/test-robbin/script.js
+++ login/test-robbin/script.js
@@ -18,17 +18,19 @@ let usersObject = {
             let usernameValue = document.forms["signIn"]["username"].value;
             let passwordValue = document.forms["signIn"]["password"].value;
             
-            this.usersArray.forEach(function(element) {
-                
+            this.usersArray.forEach(function(element) {                
                 if (usernameValue == "" || passwordValue == "") {
                     alert("Du måste fylla i användarnamn och lösenord!");
-                    return
+                    // valid = false;
+                    // break;
                 } else if (usernameValue ==  element[0] && passwordValue == element[1]) {
                     alert("Rätt användarnamn och lösenord");
-                    return
+                    // valid = true;
+                    // break;
                 } else if (usernameValue != element[0] && passwordValue != element[1]) {
                     alert("Antigen var användarnamnet eller lösenordet fel! Försök igen!");
-                    return
+                    // valid = false;
+                    // break;
                 }
                     
             }, this);
