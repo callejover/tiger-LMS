51c8384229b65abf97b0284e255ca62cdd8331e4
robba86 <robbineriksson@gmail.com>
2017-10-03 10:45:13 +0200

    Added student.html landingpage and quick-evaluation

---
+++ js/quick-evaluation.js
@@ -0,0 +1,12 @@
+// #quick-evaluation-thank-you
+
+/* #glad
+    Text: vad roligt att det är som du förväntat dig, tack för din feedback!
+*/
+/* #between
+    Text: hoppas vi kan leva upp till dina förväntingar i framtiden, tack för din feedback!
+*/
+/* #sad
+    Text: tråkig att höra att det inte är som du förväntat dig, tack för din feedback!
+*/
+
