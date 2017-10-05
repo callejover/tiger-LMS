a959d30f46bd476eb8a0185d023f0146717b853f
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2017-10-05 11:16:52 +0200

    adding confirm box for submit button

---
+++ js/feedback.js
@@ -0,0 +1,9 @@
+
+function check() { 									
+    if (confirm("Vill du skicka dina svar nu?") == true) {
+        alert("Tack för dina svar!");
+        window.open('../index.html');       
+    } 
+    else {
+    }
+}
