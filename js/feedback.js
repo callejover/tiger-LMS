bc0f123c9747757570968bcef8977e5d9ccdc2ce
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2017-10-05 13:35:00 +0200

    Changed classes in feedback-html and removed confirm-box in feedback-js

--- js/feedback.js
+++ js/feedback.js
@@ -1,9 +1,11 @@
 
 function check() { 									
-    if (confirm("Vill du skicka dina svar nu?") == true) {
+    /* if (confirm("Vill du skicka dina svar nu?") == true) {
         alert("Tack för dina svar!");
         window.open('../html/courses.html');       
     } 
     else {
-    }
+    } */
+    alert("Tack för dina svart!");
+    window.open('../html/courses.html');
 }
