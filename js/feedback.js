add356825966a51cb87d8262fd5a3bc299c0f10f
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2017-10-11 14:29:34 +0200

    Redirect weekly-feedback to courses.html

--- js/feedback.js
+++ js/feedback.js
@@ -1,11 +1,5 @@
 function check() {
-	/* var answer = confirm("Vill du skicka dina svar nu?")
-	if (answer){ */
-		alert("Tack för dina svar!");
-		window.history.back();
-	}
-/* 	else{
-		return false;
-	}
-} */
 
+		alert("Tack för dina svar!");
+		/* window.history.back(); */
+	} 
