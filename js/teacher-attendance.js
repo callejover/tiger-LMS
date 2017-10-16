a126a12a23345babfbe5636f4f8951b9248e5767
callejover <32063535+callejover@users.noreply.github.com>
2017-10-16 20:09:01 +0200

    Creates small js that ables the user to get a random code

---
+++ js/teacher-attendance.js
@@ -0,0 +1,12 @@
+//Get the choosen class value
+var choosenClass = document.getElementById('teacher-code');
+
+//Get the teachers code button
+var teacherCode = document.getElementById('code-submit');
+
+
+var randomCode = function() {
+    Math.floor((Math.random() * 9999) + 1111);
+    return;
+}
+
