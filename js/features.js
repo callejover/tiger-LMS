280973ffbd4bbbb1b6e30ff09271e31096dd3918
Rawnney <hugo.rune@me.com>
2017-10-17 15:05:33 +0200

    scrolling in features

--- js/features.js
+++ js/features.js
@@ -1,64 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function($){
+      var parPosition = [];
+      $('.par').each(function() {
+          parPosition.push($(this).offset().top);
+      });
 
-    /**
-     * This part does the "fixed navigation after scroll" functionality
-     * We use the jQuery function scroll() to recalculate our variables as the
-     * page is scrolled/
-     */
-    $(window).scroll(function(){
-        var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
-        var div_top = $('#nav-anchor').offset().top;
-            if (window_top > div_top) {
-                $('nav').addClass('stick');
-            } else {
-                $('nav').removeClass('stick');
-            }
-    });
+  $('a').click(function(){
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
 
-    /**
-     * This part causes smooth scrolling using scrollto.js
-     * We target all a tags inside the nav, and apply the scrollto.js to it.
-     */
-    $("nav a").click(function(evn){
-        evn.preventDefault();
-        $('html,body').scrollTo(this.hash, this.hash);
-    });
+      $(document).on('scroll', function() {
+          var position = $(document).scrollTop(),
+              index;
 
-    /**
-     * This part handles the highlighting functionality.
-     * We use the scroll functionality again, some array creation and
-     * manipulation, class adding and class removing, and conditional testing
-     */
-    var aChildren = $("nav li").children(); // find the a children of the list items
-    var aArray = []; // create the empty aArray
-    for (var i=0; i < aChildren.length; i++) {
-        var aChild = aChildren[i];
-        var ahref = $(aChild).attr('href');
-        aArray.push(ahref);
-    } // this for loop fills the aArray with attribute href values
+          for (var i=0; i<parPosition.length; i++) {
+              if (position <= parPosition[i]) {
+                  index = i;
+                  break;
+              }
+          }
 
-    $(window).scroll(function(){
-        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
-        var docHeight = $(document).height();
+          $('.scrollHead ul li a').removeClass('active');
+          $('.scrollHead ul li a:eq('+index+')').addClass('active');
+      });
 
-        for (var i=0; i < aArray.length; i++) {
-            var theID = aArray[i];
-            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
-            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                $("a[href='" + theID + "']").addClass("nav-active");
-            } else {
-                $("a[href='" + theID + "']").removeClass("nav-active");
-            }
-        }
-
-        if(windowPos + windowHeight == docHeight) {
-            if (!$("nav li:last-child a").hasClass("nav-active")) {
-                var navActiveCurrent = $(".nav-active").attr("href");
-                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-                $("nav li:last-child a").addClass("nav-active");
-            }
-        }
-    });
+        $('.scrollHead ul li a').click(function () {
+    $('.scrollHead ul li a').removeClass('active');
+      $(this).addClass('active');
+  });
 });
