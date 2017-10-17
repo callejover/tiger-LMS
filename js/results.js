89fc5472fcd8ddd8cd8ca2956265dc2355905f51
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2017-10-17 15:42:54 +0200

    s.15 adding more graphs

--- js/results.js
+++ js/results.js
@@ -7,6 +7,9 @@
       // Set a callback to run when the Google Visualization API is loaded.
       google.charts.setOnLoadCallback(drawChart);
       google.charts.setOnLoadCallback(chart2);
+      google.charts.setOnLoadCallback(chart3);
+      google.charts.setOnLoadCallback(chart4);
+
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
@@ -79,3 +82,77 @@
                 var chart = new google.visualization.ColumnChart(document.getElementById('chart2'));
                 chart.draw(data, options);
               }
+
+              function chart3() {
+                
+                        // Create the data table.
+                        var data = new google.visualization.DataTable();
+                        data.addColumn('string', 'Betyg');
+                        data.addColumn('number', 'Antal svar');
+                        data.addRows([
+                          ['1', 1],
+                          ['2', 4],
+                          ['3', 5],
+                          ['4', 4],
+                          ['5', 8],
+                          ['6', 2],
+                          ['7', 6],
+                        ]);
+                        
+                
+                        // Set chart options
+                        var options = {'title':'Hur engagerad anser du själv att du är i dina studier?',
+                                        colors: ['#DD46E8'],
+                                        backgroundColor: { fill:'transparent' },
+                                        legendTextStyle: { color: '#FFF' },
+                                        titleTextStyle: { color: '#FFF', fontSize: '18' },
+                                        hAxis: {
+                                        textStyle:{color: '#FFF'}
+                                        },
+                                        vAxis: {
+                                        textStyle:{color: '#FFF'}, baseline: {color: '#FFF'}
+                                        },
+                                       'width':800,
+                                       'height':300};
+                
+                        // Instantiate and draw our chart, passing in some options.
+                        var chart = new google.visualization.ColumnChart(document.getElementById('chart3'));
+                        chart.draw(data, options);
+                      }
+
+                      function chart4() {
+                        
+                                // Create the data table.
+                                var data = new google.visualization.DataTable();
+                                data.addColumn('string', 'Betyg');
+                                data.addColumn('number', 'Antal svar');
+                                data.addRows([
+                                  ['1', 0],
+                                  ['2', 2],
+                                  ['3', 5],
+                                  ['4', 8],
+                                  ['5', 8],
+                                  ['6', 2],
+                                  ['7', 5],
+                                ]);
+                                
+                        
+                                // Set chart options
+                                var options = {'title':'Hur upplever du stämningen i klassen?',
+                                                colors: ['#DD46E8'],
+                                                backgroundColor: { fill:'transparent' },
+                                                legendTextStyle: { color: '#FFF' },
+                                                titleTextStyle: { color: '#FFF', fontSize: '18' },
+                                                hAxis: {
+                                                textStyle:{color: '#FFF'}
+                                                },
+                                                vAxis: {
+                                                textStyle:{color: '#FFF'}, baseline: {color: '#FFF'}
+                                                },
+                                               'width':800,
+                                               'height':300};
+                        
+                                // Instantiate and draw our chart, passing in some options.
+                                var chart = new google.visualization.ColumnChart(document.getElementById('chart4'));
+                                chart.draw(data, options);
+                              }
