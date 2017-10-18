727107e5a60a023c26ddd3de01ce712fc16286ca
callejover <32063535+callejover@users.noreply.github.com>
2017-10-18 10:03:11 +0200

    Updates the teacher attendance to include lesson

--- js/teacher-attendance.js
+++ js/teacher-attendance.js
@@ -9,11 +9,18 @@
     //Finds the dropdowns optiions, then finds the class and returns the value
     var selectedClass = y[x].value;
 
+    //Chooses the selected option in the dropdown
+    var xx = document.querySelector("#lesson-code").selectedIndex;
+    //Creates the dropdowns options to an array
+    var yy = document.querySelector("#lesson-code").options;
+    //Finds the dropdowns optiions, then finds the class and returns the value
+    var selectLesson = yy[xx].text;
+
     //Sets the random number between 1111 and 9999
     var randomCode = getRndInteger(1111,9999);
 
     //Creates the new modals content after you've clicked on create code
-    document.querySelector(".header").innerHTML = "Den nya lektionskoden för <strong>" + selectedClass + "</strong> är <br><h2>" + randomCode +"</h2>";
+    document.querySelector(".header").innerHTML = "Den nya lektionskoden för <strong>" + selectedClass + "</strong> och <strong>" + selectLesson + "</strong> är <br><h2>" + randomCode +"</h2>";
 }
 
 
