5aa615a09f78a2ddf0b0da70ffbd1552e4ba55f1
Korkken <lappalainentim@gmail.com>
2017-10-18 10:49:13 +0200

    removed alerts from evaluations

--- js/course-evaluation.js
+++ js/course-evaluation.js
@@ -1,4 +1,15 @@
 function check() {
 	
-		alert("Tack för dina svar!");
+		document.querySelector(".course-evaluation").innerHTML = "";
+		document.querySelector(".course-evaluation").innerHTML = "TACK FÖR DINA SVAR";
+		setTimeout("pageSwap()",2000);
 }
+function pageSwap() {
+	window.location.replace("../html/courses.html");
+}
+function checkWeekly() {
+	document.querySelector(".weekly-feedback").innerHTML = "";
+	document.querySelector(".weekly-feedback").innerHTML = "TACK FÖR DINA SVAR";
+	setTimeout("pageSwap()",2000);
+}
+
