39191fad47755c0ecfc53a4f01e986f9768879fb
robba86 <robbineriksson@gmail.com>
2017-10-20 14:31:27 +0200

    S16: Adds responsive layout for table in assigment submition and over all design

--- js/assignment.js
+++ js/assignment.js
@@ -1,16 +1,16 @@
 // Get the modal
-var modal = document.getElementById('assignmentModal');
+var assignmentModal = document.querySelector('#assignmentModal');
 
 // Get the button that opens the modal
-var btn = document.querySelectorAll(".assignmentButton");
+var assignmentButton = document.querySelectorAll(".assignmentButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+var assignmentClose = document.querySelectorAll(".assignmentClose");
 
 function checkAssignment() {
     /*var inputattendance = document.getElementById("").value*/
-    var wrongInput = "<strong>Du måste skicka med ett dokument för att lämna in din uppgift!</strong>";
-    var rightInput = "<h2>Tack din uppgift är inlämnad och granskas av läraren.</h2><p>Du kommer antigen få feedback från läraren eller så får du ett betyg. Dessa kan du se direkt i tabellen över uppgifter. Denna ruta stängs automatiskt efter 10 sekunder</p>";
+    var wrongInput = "<strong>Du måste skicka med ett dokument för att lämna ditt arbete!</strong>";
+    var rightInput = "<h2>Tack för din inlämning</h2> <p>Du kommer antigen få feedback från läraren eller så får du ett betyg. Dessa kan du se direkt i tabellen med uppgifter/examinationsmoment.</p><p>Klicka på krysset eller utanför rutan för att stänga eller annars stängs den efter 30 sekunder.</p>";
     var headerText = document.querySelectorAll(".header");
 
     event.preventDefault();
@@ -18,51 +18,52 @@ function checkAssignment() {
     if (document.querySelector("#input-assignment").value === "") {
         document.querySelector(".wrongCodeAssignmnet").innerHTML = wrongInput;
         return false;
-    } else {
-
-            // Get the <span> element that closes the modal
-            
-
+    } else {   
             // When the user clicks on <span> (x), close the modal
-            for (let i = 0; i < btn.length; i++) {       
+            for (let i = 0; i < assignmentButton.length; i++) {       
                 headerText[i].onclick = function() {
                     headerText[i].innerHTML = "";
                     headerText[i].innerHTML = rightInput;
-                    setTimeout("pageSwap()",10000);
+                    setTimeout("pageSwap()",30000);
                 }
             }
-
         return true;
     }
     return false;
 }
 
 
-function pageSwap() {
-
-    modal.style.display = "none";
-}
 
 // Opens modul window to assignment
-
-
-
 // When the user clicks on the button, open the modal
-for (let i = 0; i < btn.length; i++) {
-    btn[i].onclick = function() {
-        modal.style.display = "block";
+for (let i = 0; i < assignmentButton.length; i++) {
+    assignmentButton[i].onclick = function() {
+        assignmentModal.style.display = "block";
     }    
 }
 
 // When the user clicks on <span> (x), close the modal
-for (let i = 0; i < btn.length; i++) {       
-    span[i].onclick = function() {
-        modal.style.display = "none";
+for (let i = 0; i < assignmentButton.length; i++) {       
+    assignmentClose[i].onclick = function() {
+        assignmentModal.style.display = "none";
     }
 }
+
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+window.onclick = function (event) {
+    if (event.target == assignmentModal) {
+        assignmentModal.style.display = "none";
     }
+}
+
+
+
+
+function pageSwap() {
+    // Close module
+    //modal.style.display = "none";
+
+    // Redirect to page
+    window.location.replace("../html/courses.html");
 }
--- js/attendance.js
+++ js/attendance.js
@@ -1,3 +1,7 @@
+
+
+
+
 // Checks if the attendance code valid
 
 function checkAttendance() {
@@ -28,31 +32,35 @@ function checkAttendance() {
 // Opens modul window to attendance
 
 // Get the modal
-var modal = document.getElementById('attendanceModal');
+var attendanceModal = document.getElementById('attendanceModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("attendanceButton");
+var attendanceButton = document.getElementById("attendanceButton");
+
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+close.onclick = function () {
+    attendanceModal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal
+attendanceButton.onclick = function () {
+    attendanceModal.style.display = "block";
 }
 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == attendanceModal) {
+        attendanceModal.style.display = "none";
     }
 }
+
 
 
 document.getElementById("input-attendance").addEventListener("keyup", function (event) {
