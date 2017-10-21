559297f22485c646bf603d3c041d66acb4cfe546
robba86 <robbineriksson@gmail.com>
2017-10-21 16:35:37 +0200

    Adds pageSwap when clicking outside window if form is filled correct and a timer function when comfirm message displays and pageSwap after 30 seconds

--- js/assignment.js
+++ js/assignment.js
@@ -1,6 +1,3 @@
-
-
-
 // For the modal
 var assignmentModal = document.querySelector("#assignmentModal"); // Get the modal
 var assignmentButton = document.querySelectorAll(".assignmentButton"); // Get the button that opens the modal
@@ -15,39 +12,48 @@ var assignmentRightInput = "<h2>Tack för din inlämning</h2> <p>Du kommer antig
 
 console.log(assignmentButton);
 
-            function checkAssignment() {
-                /*var inputassignment = document.getElementById("").value*/
+function checkAssignment() {
+    /*var inputassignment = document.getElementById("").value*/
 
-                event.preventDefault();
+    event.preventDefault();
 
-                if (assignmentInput.value == "") {
-                    assignmentWrongCode.innerHTML = assignmentWrongInput;
-                    return false;
-                } else {
+    if (assignmentInput.value == "") {
+        assignmentWrongCode.innerHTML = assignmentWrongInput;
+        return false;
+    } else {
 
-                    assignmentHeader.innerHTML = "";
-                    assignmentHeader.innerHTML = assignmentRightInput;
-                    setTimeout("pageSwap()", 30000);
-                }
-            }
+        assignmentHeader.innerHTML = "";
+        assignmentHeader.innerHTML = assignmentRightInput;
+        setTimeout("pageSwap()", 30000);
+    }
+}
 
 // Opens modal window to assignment
-    // When the user clicks on the button, open the modal
-    for (i = 0; i < assignmentButton.length; i++) {
-        assignmentButton[i].onclick = function () {
-            assignmentModal.style.display = "block";
-        };
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    assignmentClose.onclick = function () {
-        assignmentModal.style.display = "none";
+// When the user clicks on the button, open the modal
+for (i = 0; i < assignmentButton.length; i++) {
+    assignmentButton[i].onclick = function () {
+        assignmentModal.style.display = "block";
     };
+}
 
-    // When the user clicks anywhere outside of the modal, close it
-    /// Gets called in attendance.js
-    function windowCloseAssignmentModal() {
-        if (event.target === assignmentModal) {
-            assignmentModal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+assignmentClose.onclick = function () {
+    assignmentModal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+/// Gets called in attendance.js
+function windowCloseAssignmentModal() {
+    if (event.target === assignmentModal) {
+        assignmentModal.style.display = "none";
+        if (assignmentInput.value != "") {
+            pageSwap();
         }
     }
+}
+
+
+// Redirects to same page when added grade
+function pageSwap() {
+    window.location.replace("../html/courses.html");
+}
