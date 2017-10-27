f941ce7d36dcc911398aea45a6eeab617f6f7830
callejover <32063535+callejover@users.noreply.github.com>
2017-10-27 15:45:27 +0200

    Changes lesson code for teacher to lesson date and makes that value work in javascript

--- js/teacherAttendanceCode.js
+++ js/teacherAttendanceCode.js
@@ -9,18 +9,21 @@
     //Finds the dropdowns optiions, then finds the class and returns the value
     var selectedClass = y[x].value;
 
+    var lessonDay = document.querySelector("#lesson-day").value;
+
     //Chooses the selected option in the dropdown
-    var xx = document.querySelector("#lesson-code").selectedIndex;
+    // var xx = document.querySelector("#lesson-code").selectedIndex;
     //Creates the dropdowns options to an array
-    var yy = document.querySelector("#lesson-code").options;
+    // var yy = document.querySelector("#lesson-code").options;
     //Finds the dropdowns optiions, then finds the class and returns the value
-    var selectLesson = yy[xx].text;
+    // var selectLesson = yy[xx].text;
 
     //Sets the random number between 1111 and 9999
     var randomCode = getRndInteger(1111,9999);
 
     //Creates the new modals content after you've clicked on create code
-    document.querySelector(".header").innerHTML = "Den nya lektionskoden <br><h4>" + selectedClass + "</h4><h4>" + selectLesson + "</h4><br><h2>" + randomCode +"</h2>";
+    // document.querySelector(".header").innerHTML = "Skapa" + randomCode + "Skapa" + selectedClass + "Skapa" + lessonDay;
+    document.querySelector(".header").innerHTML = "Din närvarokod för klass. <br><h4>" + selectedClass + "</h4><br>Lektion vid datum.<br><h4>" + lessonDay + ".</h4><br><h2>" + randomCode; ".</h2>"
 }
 
 
@@ -28,3 +31,7 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+
+/* <h4>" + selectedClass + "</h4> */
+// "Den nya lektionskoden <br><h4>" + selectLesson + "</h4><br><h2>" + randomCode +"</h2>";
