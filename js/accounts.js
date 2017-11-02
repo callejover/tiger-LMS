f290a5ec6845272f36481359d39c2e629e252ecb
callejover <32063535+callejover@users.noreply.github.com>
2017-11-02 11:04:33 +0100

    Sorts up the button and response text

--- js/accounts.js
+++ js/accounts.js
@@ -1,6 +1,6 @@
 
 
 function response() {
-    document.querySelector("#accout-respond-box").innerHTML = "Grattis!<br>Du har skapat ett nytt konto!<br>Ett mail har skickats med en aktiveringskod till den nya användarens e-postadress.";
+    document.querySelector("#accout-respond-box").innerHTML = "Grattis!<br>Du har skapat ett nytt konto!<br>Ett mail har skickats med en <strong>aktiveringskod</strong> till den nya användarens e-postadress.";
 
 }
