272210ab0d27060b2eec32ec57d104108f9575f3
robba86 <robbineriksson@gmail.com>
2017-11-09 16:24:36 +0100

    Adds form on create.html, not finihed

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,3 @@
-
-
 function response() {
     document.querySelector("#accout-respond-box").innerHTML = "Grattis!<br>Du har skapat ett nytt konto!<br>Ett mail har skickats med en <strong>aktiveringskod</strong> till den nya användarens e-postadress.";
-
 }
