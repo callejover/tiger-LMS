c324de70526ba82dd2c38edcc21499f72a683c86
robba86 <robbineriksson@gmail.com>
2017-11-20 12:38:22 +0100

    Fixes modals width and input inside of them. Fixes results charts for mobile view

--- js/login.js
+++ js/login.js
@@ -1,30 +1,26 @@
-
-
 // Checks witch page youre currently at and pops out the html
 
 var currentPage = window.location.pathname.split('/').pop(); // Split will split the path into different obijects
 
 // Accouts with acess to the page
 
-var objPeople = [
-    {
-        username: "student",
-        password: "student",
-        account: "student",
-        refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/student/courses.html' : 'student/courses.html',
+var objPeople = [{
+    username: "student",
+    password: "student",
+    account: "student",
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/student/courses.html' : 'student/courses.html',
 
-    }, {
-        username: "teacher",
-        password: "teacher",
-        account: "teacher",
-        refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/teacher/courses.html' : 'teacher/courses.html'
-    }, {
-        username: "admin",
-        password: "admin",
-        account: "admin",
-        refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/admin/adminStart.html' : 'admin/adminStart.html'  // Html waiting for page
-    }
-]
+}, {
+    username: "teacher",
+    password: "teacher",
+    account: "teacher",
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/teacher/courses.html' : 'teacher/courses.html'
+}, {
+    username: "admin",
+    password: "admin",
+    account: "admin",
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/admin/adminStart.html' : 'admin/adminStart.html' // Html waiting for page
+}]
 
 // Puts typed username and password into variables
 
@@ -34,12 +30,12 @@ function getInfo() {
     var message = "Du har skrivit in fel lösenord!";
 
 
-// Loops accouts and checks for matches
-    for(i = 0; i < objPeople.length; i++) {
-        if(username == objPeople[i].username && password == objPeople[i].password) {
-            if(objPeople[i].account == "student") {
-              window.location.replace(objPeople[i].refPage);
-            } else if(objPeople[i].account == "teacher") {
+    // Loops accouts and checks for matches
+    for (i = 0; i < objPeople.length; i++) {
+        if (username == objPeople[i].username && password == objPeople[i].password) {
+            if (objPeople[i].account == "student") {
+                window.location.replace(objPeople[i].refPage);
+            } else if (objPeople[i].account == "teacher") {
                 window.location.replace(objPeople[i].refPage);
             } else {
                 window.location.replace(objPeople[i].refPage);
@@ -49,8 +45,7 @@ function getInfo() {
 
     }
 
-
-document.querySelector('#wrongPass').innerHTML = message;
+    document.querySelector('#wrongPass').innerHTML = message;
 
 }
 
@@ -70,17 +65,17 @@ var btn = document.getElementById("loginButton");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
@@ -88,7 +83,7 @@ window.onclick = function(event) {
 
 
 
-document.getElementById("password").addEventListener("keyup", function(event) {
+document.getElementById("password").addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.keyCode == 13) {
         document.getElementById("password").click(getInfo());
