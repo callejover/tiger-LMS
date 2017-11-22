b130dfd32a00ddcdaf2eb7a660eac09b605e6213
Hugo Rune <timaro@live.se>
2017-11-22 10:21:30 +0100

    testing hiding menu

--- js/navMenu.js
+++ js/navMenu.js
@@ -7,10 +7,10 @@ let hiddenNavWarp = document.querySelector(".hidden-max-500");          // Gets 
 
 window.onload = function() {
     if (window.innerWidth < 1200) {
-        hiddenNavWarp.style.display = "none";     
+        hiddenNavWarp.style.display = "none";
     } else {
-        hiddenNavWarp.style.display = "flex";       
-    } 
+        hiddenNavWarp.style.display = "flex";
+    }
 }
 
 /*
@@ -20,28 +20,28 @@ window.onload = function() {
 
 function navShowHide() { // Change .hidden-max-500 element to display: flex
     if (hiddenNavWarp.style.display == "flex") {
-        hiddenNavWarp.style.display = "none";     
+        hiddenNavWarp.style.display = "none";
     } else {
-        hiddenNavWarp.style.display = "flex";       
+        hiddenNavWarp.style.display = "flex";
     }
 }
 
 /*
     When resizing the window and the menu is toggled and hidden
-    Reset the state so it aper on the desktop view again 
+    Reset the state so it aper on the desktop view again
 */
 function resizeWindow() {
     if (window.innerWidth < 1200) {
-        hiddenNavWarp.style.display = "none";     
+        hiddenNavWarp.style.display = "none";
     } else {
-        hiddenNavWarp.style.display = "flex";       
-    } 
-    
+        hiddenNavWarp.style.display = "flex";
+    }
+
 }
 
 
 
-// Call functions with event listener 
+// Call functions with event listener
 window.addEventListener("resize", resizeWindow);
 hiddenNavButton.addEventListener("click", navShowHide);
 
@@ -65,3 +65,101 @@ function menuButton(x) {
 function goBack() {
 	window.history.back(); //Allows the user to return to recent page
 }
+
+
+// =========================================================================================================================
+// Auto hide menu
+// =========================================================================================================================
+
+
+jQuery(document).ready(function($){
+	var mainHeader = $('.cd-auto-hide-header'),
+		secondaryNavigation = $('.cd-secondary-nav'),
+		//this applies only if secondary nav is below intro section
+		belowNavHeroContent = $('.sub-nav-hero'),
+		headerHeight = mainHeader.height();
+
+	//set scrolling variables
+	var scrolling = false,
+		previousTop = 0,
+		currentTop = 0,
+		scrollDelta = 10,
+		scrollOffset = 150;
+
+	mainHeader.on('click', '.nav-trigger', function(event){
+		// open primary navigation on mobile
+		event.preventDefault();
+		mainHeader.toggleClass('nav-open');
+	});
+
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			scrolling = true;
+			(!window.requestAnimationFrame)
+				? setTimeout(autoHideHeader, 250)
+				: requestAnimationFrame(autoHideHeader);
+		}
+	});
+
+	$(window).on('resize', function(){
+		headerHeight = mainHeader.height();
+	});
+
+
+	function autoHideHeader() {
+		var currentTop = $(window).scrollTop();
+
+		( belowNavHeroContent.length > 0 )
+			? checkStickyNavigation(currentTop) // secondary navigation below intro
+			: checkSimpleNavigation(currentTop);
+
+	   	previousTop = currentTop;
+		scrolling = false;
+	}
+
+	function checkSimpleNavigation(currentTop) {
+		//there's no secondary nav or secondary nav is below primary nav
+	    if (previousTop - currentTop > scrollDelta) {
+	    	//if scrolling up...
+	    	mainHeader.removeClass('is-hidden');
+	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+	    	//if scrolling down...
+	    	mainHeader.addClass('is-hidden');
+	    }
+	}
+
+	function checkStickyNavigation(currentTop) {
+		//secondary nav below intro section - sticky secondary nav
+		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
+
+		if (previousTop >= currentTop ) {
+	    	//if scrolling up...
+	    	if( currentTop < secondaryNavOffsetTop ) {
+	    		//secondary nav is not fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('fixed slide-up');
+	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
+	    	} else if( previousTop - currentTop > scrollDelta ) {
+	    		//secondary nav is fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('slide-up').addClass('fixed');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	}
+
+	    } else {
+	    	//if scrolling down...
+	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
+	 	  		//hide primary nav
+	    		mainHeader.addClass('is-hidden');
+	    		secondaryNavigation.addClass('fixed slide-up');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	} else if( currentTop > secondaryNavOffsetTop ) {
+	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	}
+
+	    }
+	}
+});
